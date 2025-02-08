e3baceea10458bc2d533a33eac09724dacb6ebba
Tiến <letien3413@gmail.com>
2025-02-08 10:22:42 +0700

    tạo layout trang chủ admin

---
+++ resources/views/admin/layout_admin/script.blade.php
@@ -0,0 +1,12 @@
+<!--   Core JS Files   -->
+<script src="/admin/assets/js/core/jquery-3.7.1.min.js"></script>
+<script src="/admin/assets/js/core/popper.min.js"></script>
+<script src="/admin/assets/js/core/bootstrap.min.js"></script>
+
+<!-- jQuery Scrollbar -->
+<script src="/admin/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+
+<!-- Datatables -->
+<script src="/admin/assets/js/plugin/datatables/datatables.min.js"></script>
+
+<script src="/admin/assets/js/kaiadmin.min.js"></script>
